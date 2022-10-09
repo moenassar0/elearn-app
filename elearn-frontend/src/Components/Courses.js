162da70a3d9b44b76e88e67a480c2a05a87bb98f
Mohamad Nassar <mnassar57@gmail.com>
2022-10-09 06:56:55 +0300

    frontend: fetched data from database

--- elearn-frontend/src/Components/Courses.js
+++ elearn-frontend/src/Components/Courses.js
@@ -12,6 +12,11 @@ export function Courses() {
         fetchData()
     }, [])
 
+    const fetchData = async () => {
+        const response = await axios.get('http://127.0.0.1:8000/api/courses');
+        console.log(response.data.courses)
+        getData(response.data.courses);
+    }
 
     return (
         <div className='container'>
@@ -30,7 +35,13 @@ export function Courses() {
                                 <th>Course Name</th>
                                 <th>Description</th>
                             </tr>
-
+                            {data.map((item, i) => (
+                                <tr key={i}>
+                                    <td>{item.course_code}</td>
+                                    <td>{item.course_name}</td>
+                                    <td>{item.course_description}</td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
