e90da4fa1175224bb53581c2b61236b0b60351fe
Mohamad Nassar <mnassar57@gmail.com>
2022-10-09 07:00:30 +0300

    frontend: update data for parent container

--- elearn-frontend/src/Components/AddStudentPopup.js
+++ elearn-frontend/src/Components/AddStudentPopup.js
@@ -15,7 +15,11 @@ function AddStudentPopup(props){
     const [errMsg, setErrMsg] = useState('');
     
 
-
+    const fetchData = async () => {
+        const response = await axios.get('http://127.0.0.1:8000/api/auth/users');
+        console.log(response.data.instructors)
+        props.setData(response.data.instructors);
+    }
 
 
         const handleSubmit = async (e) => {
