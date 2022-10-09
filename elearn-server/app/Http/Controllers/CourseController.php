e5ccc52a05e236becf93f9a40c552352745d5b61
Mohamad Nassar <mnassar57@gmail.com>
2022-10-09 07:07:37 +0300

    backend: added add course function

--- elearn-server/app/Http/Controllers/CourseController.php
+++ elearn-server/app/Http/Controllers/CourseController.php
@@ -7,6 +7,22 @@ use App\Models\Course;
 
 class CourseController extends Controller
 {
+    public function addCourse(Request $request)
+    {
+        if(!auth()->user()){
+            return response()->json(["result" => 'not authurized']);
+        }
+        $course = new Course;
+ 
+        $course->course_name = $request->course_name;
+        $course->course_description = $request->course_description;
+        $course->course_code = $request->course_code;
+
+        $course->save();
+ 
+        return response()->json(["result" => "ok", 'course added:' => $course], 201);
+    }
+
     public function getCurses(){
         $courses = Course::select('*')->get();
         return response()->json(['courses' => $courses], 201);
