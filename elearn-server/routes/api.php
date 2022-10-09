b690abc6ba5b8c44cd6491553525a8f0b50bab84
Mohamad Nassar <mnassar57@gmail.com>
2022-10-09 07:09:52 +0300

    backend: added starter api routes

--- elearn-server/routes/api.php
+++ elearn-server/routes/api.php
@@ -2,18 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CourseController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post("/add", [UserController::class, "store"]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get("/courses", [CourseController::class, "getCurses"]);
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function($router) {
+    Route::post("/register", [AuthController::class, "register"]);
+    Route::post("/login", [AuthController::class, "login"]);
+    Route::get("/users", [UserController::class, "getStudents"]);
+    Route::post("/addcourse", [CourseController::class, "addCourse"]);
+    Route::post("/me", [AuthController::class, "me"]);
+    
+   
+});
+
+
+
