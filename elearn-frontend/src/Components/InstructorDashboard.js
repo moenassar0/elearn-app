a824f82964199225f9969ca2cbef83494821e3f3
Mohamad Nassar <mnassar57@gmail.com>
2022-10-09 06:55:26 +0300

    frontend: dummy instructor page added

---
+++ elearn-frontend/src/Components/InstructorDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useRoutes,
+  } from "react-router-dom";
+  
+
+export function InstructorDashboard() {
+  return(
+    <h2>InstructorDashboard</h2>
+  );
+}
