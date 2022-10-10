b93dfce91501851d8b08d9cadb3b27b46ae119bf
Mohamad Nassar <mnassar57@gmail.com>
2022-10-11 02:23:14 +0300

    frontend: added student navbar

---
+++ elearn-frontend/src/Components/StudentNavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
+
+export function StudentNavBar() {
+  return(
+    <>
+    <div className='admin-navbar'>
+        <Link className='navbar-item' to="/student/courses">
+            <div><img className='img-resize' src='../../images/courses_icon.svg'></img></div>
+            <div className="overflow-text">Schedule</div>
+        </Link>
+        <div className='logout-div'>
+            <button>Logout</button>
+        </div>
+    </div>
+    </>
+  );
+}
