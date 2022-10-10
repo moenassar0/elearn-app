7bf8a841ffe4ba5793508b83123429e29467ba05
Mohamad Nassar <mnassar57@gmail.com>
2022-10-10 08:36:39 +0300

    frontend: updated instructor page

--- elearn-frontend/src/Components/InstructorSchedule.js
+++ elearn-frontend/src/Components/InstructorSchedule.js
@@ -4,11 +4,13 @@ import axios, { Axios } from '../api/axios';
 import PropTypes from 'prop-types';
 import { Navigate } from 'react-router-dom';
 import EnrollStudentPopup from './EnrollStudentPopup';
+import ShowEnrolledStudentsPopup from './ShowEnrolledStudentsPopup';
 
 export function InstructorSchedule() {
 
     const [data, getData] = useState([])
     const [enrollStudentButton, setEnrollStudentButton] = useState(false);
+    const [showStudentsButton, setShowStudentsButton] = useState(false);
     const [currCourseID, setCurrCourseID] = useState(0);
 
     useEffect(() => {
@@ -27,11 +29,19 @@ export function InstructorSchedule() {
         setCurrCourseID(id);
     }
 
+    const showStudentsPopup = (course_id) => {
+        setShowStudentsButton(true);
+        setCurrCourseID(course_id);
+    }
+
     return (
         <div className='container'>
             <EnrollStudentPopup course_id={currCourseID} trigger={enrollStudentButton} setTrigger={setEnrollStudentButton} setData={getData}>
 
             </EnrollStudentPopup>
+            <ShowEnrolledStudentsPopup course_id={currCourseID} trigger={showStudentsButton} setTrigger={setShowStudentsButton}>
+
+            </ShowEnrolledStudentsPopup>
             <div className="main-header">
                 <div className="main-header-title">
                     <span>Check out your schedule</span>
@@ -50,7 +60,12 @@ export function InstructorSchedule() {
                                     <td>{item.course_code}</td>
                                     <td>{item.course_name}</td>
                                     <td>{item.course_description}</td>
-                                    <td><button>Add Announcement</button><button onClick={() => {startEnrollStudentPopup(item._id)}}>Enroll</button><button>Add Assignment</button></td>
+                                    <td>
+                                        <button>Add Announcement</button>
+                                        <button onClick={() => {startEnrollStudentPopup(item._id)}}>Enroll</button>
+                                        <button>Add Assignment</button>
+                                        <button onClick={() => {showStudentsPopup(item._id)}}>View Students</button>
+                                    </td>
                                 </tr>
                             ))}
                         </tbody>
