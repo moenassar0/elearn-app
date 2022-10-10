e0f5eb672e046fc4e4c669e862c5207753ad8607
Mohamad Nassar <mnassar57@gmail.com>
2022-10-10 06:57:34 +0300

    frontend: updated instructor dashboard

--- elearn-frontend/src/Components/InstructorDashboard.js
+++ elearn-frontend/src/Components/InstructorDashboard.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    useRoutes,
-  } from "react-router-dom";
+import { useRef, useState, useEffect } from 'react';
+import { InstructorNavBar } from './InstructorNavBar';
+import axios, { Axios } from '../api/axios';
+import { BrowserRouter as Router, Link, Routes, Route, Outlet, Navigate } from 'react-router-dom';
+import { Users } from './Users';
   
 
 export function InstructorDashboard() {
+    const [user_type, setUserType] = useState('Instructor')
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
+    if(localStorage.getItem("token") == undefined || localStorage.getItem("token") == ''){
+        return <Navigate to="/" />;
+    }
+
+    const fetchUser = async () => {
+        try{
+            const response = await axios.post('http://127.0.0.1:8000/api/auth/me', 
+            {}, { headers:{'Authorization' : "Bearer " + localStorage.getItem("token")}});
+            setUserType(response.data['user_type']);
+        }catch{
+            setUserType('');
+        }
+    }
+
+    if(user_type != 'Instructor'){
+        return <Navigate to="/" />;
+    }
+
   return(
-    <h2>InstructorDashboard</h2>
+    <>
+        <InstructorNavBar />
+        <Outlet />
+    </>
   );
 }
