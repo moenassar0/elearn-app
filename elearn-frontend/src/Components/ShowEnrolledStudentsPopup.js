307d85fd40884eca58e60ec52d98931aa2a4745e
Mohamad Nassar <mnassar57@gmail.com>
2022-10-11 02:04:32 +0300

    frontend: added loading icon

--- elearn-frontend/src/Components/ShowEnrolledStudentsPopup.js
+++ elearn-frontend/src/Components/ShowEnrolledStudentsPopup.js
@@ -3,37 +3,42 @@ import React from 'react'
 import axios, { Axios } from '../api/axios';
 import PropTypes from 'prop-types';
 
+
 function ShowEnrolledStudentsPopup(props){
     
     const [studentsData, setStudentsData] = useState([]);
-
+    const [call, setCall] = useState(false);
     useEffect(() => {
         fetchStudentData()
     }, [])
 
     //Get instructor's assigned courses
     const fetchStudentData = async () => {
-        const response = await axios.post('http://127.0.0.1:8000/api/auth/enrolled',
-        {course_id: props.course_id}, 
-        {headers:{'Authorization' : "Bearer " + localStorage.getItem("token")}});
-        //console.log(props.course_id);
-        setStudentsData(response.data.students);
+        try{
+            const response = await axios.post('http://127.0.0.1:8000/api/auth/enrolled',
+            {course_id: props.course_id}, 
+            {headers:{'Authorization' : "Bearer " + localStorage.getItem("token")}});
+            console.log(response);
+            console.log(props.course_id);
+            setStudentsData(response.data.students);
+            setCall(true);
+        }catch{
+        }
     }
-
-    //fetchStudentData();
 
     return (props.trigger) ? (
         <div className='popup'>
             <div className='popup-inner'>
+            {!call && <img className='img-resize' src="../../images/loading-load.gif"></img>}
                 <button onClick={() => { props.setTrigger(false)} }className='close-btn'>Close</button>
                 <table className="main-table" id="main-table-clients">
                         <tbody>
                             <tr>
-                                <th>Student Names</th>
+                                <th>List of enrolled students:</th>
                             </tr>
                             {studentsData.map((item, i) => (
                                 <tr key={item._id}>
-                                    <td>{item.f_name}</td>
+                                    <td>{item.f_name + " " + item.l_name}</td>
                                 </tr>
                             ))}
                         </tbody>
