0551fb001c06bfd2c11a113e3df75ae1160f6fc4
Mohamad Nassar <mnassar57@gmail.com>
2022-10-10 08:35:46 +0300

    frontend: updated enrolledstudents popup

--- elearn-frontend/src/Components/ShowEnrolledStudentsPopup.js
+++ elearn-frontend/src/Components/ShowEnrolledStudentsPopup.js
@@ -4,12 +4,40 @@ import axios, { Axios } from '../api/axios';
 import PropTypes from 'prop-types';
 
 function ShowEnrolledStudentsPopup(props){
-     
+    
+    const [studentsData, setStudentsData] = useState([]);
+
+    useEffect(() => {
+        fetchStudentData()
+    }, [])
+
+    //Get instructor's assigned courses
+    const fetchStudentData = async () => {
+        const response = await axios.post('http://127.0.0.1:8000/api/auth/enrolled',
+        {course_id: props.course_id}, 
+        {headers:{'Authorization' : "Bearer " + localStorage.getItem("token")}});
+        //console.log(props.course_id);
+        setStudentsData(response.data.students);
+    }
+
+    //fetchStudentData();
+
     return (props.trigger) ? (
         <div className='popup'>
             <div className='popup-inner'>
                 <button onClick={() => { props.setTrigger(false)} }className='close-btn'>Close</button>
-                <button onClick={ () => {} }>Enroll</button>
+                <table className="main-table" id="main-table-clients">
+                        <tbody>
+                            <tr>
+                                <th>Student Names</th>
+                            </tr>
+                            {studentsData.map((item, i) => (
+                                <tr key={item._id}>
+                                    <td>{item.f_name}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
             </div>
         </div>
     ) : '';
