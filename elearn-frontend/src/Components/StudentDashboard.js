4a688e61f53e350b639614d40a68700bbe75539a
Mohamad Nassar <mnassar57@gmail.com>
2022-10-11 02:04:17 +0300

    frontend: student dashboard base added

---
+++ elearn-frontend/src/Components/StudentDashboard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useRef, useState, useEffect } from 'react';
+import { AdminNavBar } from './AdminNavBar';
+import axios, { Axios } from '../api/axios';
+import { BrowserRouter as Router, Link, Routes, Route, Outlet, Navigate } from 'react-router-dom';
+
+
+
+export function StudentDashboard() {
+
+    const [user_type, setUserType] = useState('Student')
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
+    if(localStorage.getItem("token") == undefined || localStorage.getItem("token") == ''){
+        return <Navigate to="/" />;
+    }
+
+    const fetchUser = async () => {
+        try{
+            const response = await axios.post('http://127.0.0.1:8000/api/auth/me', 
+            {}, { headers:{'Authorization' : "Bearer " + localStorage.getItem("token")}});
+            setUserType(response.data['user_type']);
+        }catch{
+            setUserType('');
+        }
+    }
+
+    if(user_type != 'Student'){
+        return <Navigate to="/" />;
+    }
+
+  return(
+    <>
+        <AdminNavBar />
+        <Outlet />
+    </>
+  );
+}
