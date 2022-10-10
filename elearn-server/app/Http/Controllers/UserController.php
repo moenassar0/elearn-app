624508d182102d04cba80cb27ef83dede20a5bb2
Mohamad Nassar <mnassar57@gmail.com>
2022-10-10 06:11:24 +0300

    frontend: get instructors function added

--- elearn-server/app/Http/Controllers/UserController.php
+++ elearn-server/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Course;
 
 class UserController extends Controller
 {
@@ -26,4 +27,11 @@ class UserController extends Controller
         $users = User::where('user_type', 'Student')->get();
         return response()->json(['instructors' => $users], 201);
     }
+
+    public function getInstructors(){
+        $users = User::where('user_type', 'Instructor')->get();
+        return response()->json(['instructors' => $users], 201);
+    }
+
+
 }
