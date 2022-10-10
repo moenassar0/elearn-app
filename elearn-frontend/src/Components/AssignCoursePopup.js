7f045640d0ad3ce402d959fb3f250c62eb2b5e4e
Mohamad Nassar <mnassar57@gmail.com>
2022-10-10 06:10:27 +0300

    frontend: fetched data for instructors

--- elearn-frontend/src/Components/AssignCoursePopup.js
+++ elearn-frontend/src/Components/AssignCoursePopup.js
@@ -6,7 +6,41 @@ import { Navigate } from 'react-router-dom';
 
 function AssignCoursePopup(props){
 
-  
+    const [instructors, setInstructors] = useState([]);
+   const [selectedInstructor, setInstructor] = useState(0);
+    const assignInstructor = async (e) => {
+        e.preventDefault();
+        console.log(props.course_id, selectedInstructor);
+        try{
+            const response = await axios.post(
+                'http://127.0.0.1:8000/api/auth/assigncourse', 
+                {instructor_id: selectedInstructor, course_id: props.course_id},
+                {headers:{'Authorization' : "Bearer " + localStorage.getItem("token")}});
+            console.log(response.status != 200 || response.status != 201);
+            fetchCourses();
+        }
+        catch{
+        }
+    } 
+
+    /*const [data, getData] = useState([])
+    const [addCourseButton, setAddCourseButton] = useState(false);
+    */
+    useEffect(() => {
+        fetchData()
+    }, [])
+    
+    //Get list of instructors
+    const fetchData = async () => {
+        const response = await axios.get('http://127.0.0.1:8000/api/auth/instructors');
+        console.log(response.data.instructors)
+        setInstructors(response.data.instructors);
+    }
+
+    const fetchCourses = async () => {
+        const response = await axios.get('http://127.0.0.1:8000/api/courses');
+        props.setData(response.data.courses);
+    }      
 
 //<select id="select-voucher" class="">
 //<option value="0">Select instructor:</option>
