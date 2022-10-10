32e871175c62b67b2e38cb16a114d09ff7e70a7d
Mohamad Nassar <mnassar57@gmail.com>
2022-10-11 02:04:49 +0300

    frontend: add announcement implemented

--- elearn-frontend/src/Components/AddAnnouncementPopup.js
+++ elearn-frontend/src/Components/AddAnnouncementPopup.js
@@ -5,14 +5,24 @@ import axios, { Axios } from '../api/axios';
 function AddAnnouncementPopup(props){
 
     const [announcement, setAnnouncement] = useState('');
+    const [adding, setAdding] = useState(true);
     const userRef = useRef();
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setAdding(false);
+        const response = await axios.post(
+            'http://127.0.0.1:8000/api/auth/announcement', 
+            {course_id: props.course_id, announcement_description: announcement},
+            {headers:{'Authorization' : "Bearer " + localStorage.getItem("token")}});
+        console.log(response.status != 200 || response.status != 201);
+        console.log(response);
+        setAdding(true);
     }
 
     return (props.trigger) ? (
         <div className='popup'>
             <div className='popup-inner'>
+                {!adding && <img className='img-resize' src="../../images/loading-load.gif"></img>}
                 <button onClick={() => { props.setTrigger(false)} }className='close-btn'>Close</button>
                 <input 
                     type="text"
