02ed93a7cdb5036e96b912f2588d4d6577a35097
Mohamad Nassar <mnassar57@gmail.com>
2022-10-11 15:30:59 +0300

    frontend: code cleanup and styling

--- elearn-frontend/src/Components/AddCoursePopup.js
+++ elearn-frontend/src/Components/AddCoursePopup.js
@@ -1,67 +1,51 @@
 import { useRef, useState, useEffect } from 'react';
 import React from 'react'
-import axios, { Axios } from '../api/axios';
-import PropTypes from 'prop-types';
-import { Navigate } from 'react-router-dom';
+import axios from '../api/axios';
 
 function AddCoursePopup(props){
-
-    const userRef = useRef();
 
     const [course_name, setCourseName] = useState('');
     const [course_code, setCourseCode] = useState('');
     const [course_description, setCourseDescription] = useState('');
-    const [errMsg, setErrMsg] = useState('');
     
-
     const fetchData = async () => {
         const response = await axios.get('http://127.0.0.1:8000/api/courses');  
         props.setData(response.data.courses);
     }
 
-
-        const handleSubmit = async (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(course_name, course_code, course_description);
-        try{
-            const response = await axios.post(
-                'http://127.0.0.1:8000/api/auth/addcourse', 
-                {course_name, course_code, course_description},
-                {headers:{'Authorization' : "Bearer " + localStorage.getItem("token")}});
-            console.log(response.status != 200 || response.status != 201);
-            fetchData();
-        }catch{
-            
-        }
+        const response = await axios.post('/auth/addcourse', {course_name, course_code, course_description});
+        fetchData();
     }
 
     return (props.trigger) ? (
         <div className='popup'>
             <div className='popup-inner'>
-                <button onClick={() => { props.setTrigger(false)} }className='close-btn'>Close</button>
-                <input 
+            <div className='popup-inner-top'><button className='btn-purple' onClick={() => { props.setTrigger(false)} }>Close</button></div>
+                <input
+                    className='login-input' 
                     type="text"
                     id="username"
-                    ref={userRef}
                     autoComplete="off"
                     onChange={(e) => setCourseName(e.target.value)}
                     value={course_name}
                     placeholder="course name"
                     required
                 />
-                <input 
+                <input
+                    className='login-input' 
                     type="text"
                     id="username"
-                    ref={userRef}
                     autoComplete="off"
                     onChange={(e) => setCourseCode(e.target.value)}
                     value={course_code}
                     placeholder="course code"
                     required
                 />
-                <input 
+                <input
+                    className='login-input'
                     type="text"
-                    ref={userRef}
                     autoComplete="off"
                     onChange={(e) => setCourseDescription(e.target.value)}
                     value={course_description}
@@ -69,7 +53,7 @@ function AddCoursePopup(props){
                     required
                 />
                 
-                <button onClick={ handleSubmit }>Add</button>
+                <button className='btn-purple' onClick={ handleSubmit }>Add</button>
             </div>
         </div>
     ) : '';
