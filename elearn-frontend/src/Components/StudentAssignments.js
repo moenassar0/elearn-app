b028d3b533609b8ac97fc4e97b3a83c7b267fac6
Mohamad Nassar <mnassar57@gmail.com>
2022-10-11 04:42:15 +0300

    frontend: updated student assignments page

--- elearn-frontend/src/Components/StudentAssignments.js
+++ elearn-frontend/src/Components/StudentAssignments.js
@@ -1,3 +1,31 @@
+import { useEffect, useState } from "react";
+import axios, { Axios } from '../api/axios';
+
 export const StudentAssignments = () => {
-    return <h1>gg</h1>
+
+    const [courses, setCourses] = useState([])
+    const [showAssignments, setShowAssignments] = useState(false);
+
+    useEffect(() => {
+        fetchSchedule()
+    }, [])
+
+    function showAssignmentsDiv(course_id){
+
+    }
+
+    //Get instructor's assigned courses
+    const fetchSchedule = async () => {
+        const response = await axios.post('http://127.0.0.1:8000/api/auth/studentcourses',
+        {}, 
+        {headers:{'Authorization' : "Bearer " + localStorage.getItem("token")}});
+        setCourses(response.data.courses);
+    }
+    return (
+        <>
+            {courses.map((course) => (
+                <button onClick={() => {showAssignmentsDiv()}}>{course.course_name}</button>
+            ))}
+        </>
+    )
 }
