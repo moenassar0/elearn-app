ac62a1b7124e5b832748621d4312bf0de419bf96
Mohamad Nassar <mnassar57@gmail.com>
2022-10-11 09:28:49 +0300

    frontend: updated routes

--- elearn-frontend/src/App.js
+++ elearn-frontend/src/App.js
@@ -4,22 +4,22 @@ import './App.css';
 import { AdminDashboard } from './Components/AdminDashboard';
 import { InstructorDashboard } from './Components/InstructorDashboard';
 import { useRef, useState, useEffect } from 'react';
-import useToken from './useToken';
-import {Users} from "./Components/Users";
-import {Courses} from "./Components/Courses";
+import { Students } from "./Components/Students";
+import { Courses } from "./Components/Courses";
 import { InstructorSchedule } from "./Components/InstructorSchedule"
 import { StudentDashboard } from './Components/StudentDashboard.js';
 import { StudentSchedule } from './Components/StudentSchedule';
 import { StudentAssignments } from './Components/StudentAssignments';
+import { Instructors } from './Components/Instructors.js';
 
 function App() {
-
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />}></Route>
         <Route path="/admin" element={ <AdminDashboard /> }>
-          <Route path="/admin/users" element={<Users />}></Route>
+          <Route path="/admin/students" element={<Students />}></Route>
+          <Route path="/admin/instructors" element={<Instructors />}></Route>
           <Route path="/admin/courses" element={<Courses />}></Route>
         </Route>
         <Route path="/instructor" element={ <InstructorDashboard /> }>
