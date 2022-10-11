6e0e2d2fb8b0983bc28b87e67b8bc63362fd337d
Mohamad Nassar <mnassar57@gmail.com>
2022-10-11 03:09:32 +0300

    frontend: added student assignments route

--- elearn-frontend/src/App.js
+++ elearn-frontend/src/App.js
@@ -8,14 +8,11 @@ import useToken from './useToken';
 import {Users} from "./Components/Users";
 import {Courses} from "./Components/Courses";
 import { InstructorSchedule } from "./Components/InstructorSchedule"
+import { StudentDashboard } from './Components/StudentDashboard.js';
+import { StudentSchedule } from './Components/StudentSchedule';
+import { StudentAssignments } from './Components/StudentAssignments';
 
 function App() {
-
-  
-
-  //if(!token) {
-  //  console.log(token);
-  //}
 
   return (
     <>
@@ -28,10 +25,10 @@ function App() {
         <Route path="/instructor" element={ <InstructorDashboard /> }>
           <Route path="/instructor/courses" element={<InstructorSchedule />}></Route>
         </Route>
-        <Route path="/student" element={ <InstructorDashboard /> }>
-          <Route path="/student/courses" element={<InstructorSchedule />}></Route>
+        <Route path="/student" element={ <StudentDashboard /> }>
+          <Route path="/student/courses" element={<StudentSchedule />}></Route>
+          <Route path="/student/assignments" element={<StudentAssignments />}></Route>
         </Route>
-        
       </Routes>
     </>
   );
