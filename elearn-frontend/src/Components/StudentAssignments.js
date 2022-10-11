df40d9f42b6b27d8cd532f99223cf4893e64ecff
Mohamad Nassar <mnassar57@gmail.com>
2022-10-11 08:12:48 +0300

    frontend: code cleanup

--- elearn-frontend/src/Components/StudentAssignments.js
+++ elearn-frontend/src/Components/StudentAssignments.js
@@ -1,50 +1,32 @@
 import { useEffect, useState } from "react";
-import axios, { Axios } from '../api/axios';
+import axios from '../api/axios';
 
 export const StudentAssignments = () => {
 
     const [courses, setCourses] = useState([])
     const [showAssignments, setShowAssignments] = useState(false);
     const [assignments, setAssignments] = useState([]);
-    const [currentCourse, setCurrentCourse] = useState('');
     const [loadingForAssignments, setLoadingForAssignments] = useState(true);
 
     useEffect(() => {
         fetchSchedule()
     }, [])
 
-    async function showAssignmentsDiv(course_id){
-        setCurrentCourse(course_id);
-        await fetchAssignments();
-        
-    }
-
     //Get student's enrolled courses
     const fetchSchedule = async () => {
-        const response = await axios.post('http://127.0.0.1:8000/api/auth/studentcourses',
-        {}, 
-        {headers:{'Authorization' : "Bearer " + localStorage.getItem("token")}});
+        const response = await axios.post('/auth/studentcourses', {});
         setCourses(response.data.courses);
     }
 
     //Get assignments for specific course
     const fetchAssignments = async (course_id) => {
         setLoadingForAssignments(false);
-        const response = await axios.post('http://127.0.0.1:8000/api/auth/assignments',
-        {course_id}, 
-        {headers:{'Authorization' : "Bearer " + localStorage.getItem("token")}});
+        const response = await axios.post('/auth/assignments', {course_id});
         setAssignments(response.data.assignment);
         setShowAssignments(true);
         setLoadingForAssignments(true);
     }
-/*
-{showAssignments && 
-<table><tr>
-    {assignments.map((assignment) => (
-        <td>{assignment._id}</td>
-    ))}
-</tr></table>}
-*/
+
     return (
         <>
         <div className='container'>
@@ -54,7 +36,7 @@ export const StudentAssignments = () => {
                 </div>
                 <div>
                     {courses.map((course, i) => (
-                    <button className="btn-purple" key={i} onClick={() => {fetchAssignments(course._id)}}>{course.course_name}</button>))}
+                    <button className="main-button" key={i} onClick={() => {fetchAssignments(course._id)}}>{course.course_name}</button>))}
                 </div>
                 <div className="main-display-table">
                     <table className="main-table" id="main-table-clients">
