ffe3a60b3645adb3da7d73b20112e8882995919b
Mohamad Nassar <mnassar57@gmail.com>
2022-10-11 06:55:38 +0300

    backend: added custom middleware based on token

---
+++ elearn-server/app/Http/Middleware/JWTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use JWTAuth;
+use Exception;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+
+class JWTest extends BaseMiddleware
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(isset(auth()->user()->id))
+            return $next($request);
+        else
+            return response()->json(['status' => 'Authorization Token Error']);
+    }
+}
